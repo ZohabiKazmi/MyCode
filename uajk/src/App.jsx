a7a180dba86079903ff194d9741f38305e98e415
Zohaib Ali Shah <dev.zohaibkazmi@gmail.com>
2024-08-28 13:41:09 +0500

    First git commit

---
+++ uajk/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Hero from './components/HeroSlider/HeroSlider'
+
+
+const App = () => {
+  return (
+    <>
+    <Hero/>
+    </>
+  )
+}
+
+export default App
