da5f2274191ad9f47c4c85beb278975ea022253b
Zohaib Ali Shah <dev.zohaibkazmi@gmail.com>
2024-08-30 15:56:34 +0500

    navbar updated

--- uajk/src/components/Navbar/Navbar.jsx
+++ uajk/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import './NavBar.css'; 
 
-import logo from '../../assets/uajk-logo.png'
+import logo from '../../assets/uajk-logo.png';
 
 const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [closing, setClosing] = useState(false); // Track the closing state
 
   const handleMenuToggle = () => {
-    setMenuOpen(!menuOpen);
+    if (menuOpen) {
+      setClosing(true); // Set closing state to true when closing the menu
+      setTimeout(() => {
+        setMenuOpen(false); // Actually close the menu after animation ends
+        setClosing(false); // Reset closing state
+      }, 500); // Match the duration of the slide-out animation
+    } else {
+      setMenuOpen(true);
+    }
   };
 
   return (
@@ -21,9 +30,9 @@ const NavBar = () => {
         {/* Center Menu */}
         <div className="center-menu">
           <ul>
-            <li>About</li>
-            <li>Services</li>
-            <li>Tour</li>
+            <li>University</li>
+            <li>Academic</li>
+            <li>Admission</li>
             <li>Contact</li>
           </ul>
         </div>
@@ -34,23 +43,30 @@ const NavBar = () => {
         </div>
 
         {/* Slide-in Menu */}
-        {menuOpen && (
-          <div className="side-menu">
+        {(menuOpen || closing) && (
+          <div className={`side-menu ${closing ? 'slide-out' : 'slide-in'}`}>
             <div className="close-icon" onClick={handleMenuToggle}>
               &times;
             </div>
             <ul className="side-menu-list">
-              <li>Home</li>
-              <li>About</li>
-              <li>Services</li>
-              <li>Tour</li>
-              <li>Contact</li>
+              <li>Administrative Offices</li><hr />
+              <li>Examintation</li><hr />
+              <li>Life At University</li><hr />
+              <li>Q.E.C</li><hr />
+              <li>Online Admission</li><hr />
+              <li>Online Portal</li><hr />
+              <li>Affiliated Colleges</li><hr />
+              <li>Policies and Guidelines</li><hr />
+              <li>University Organogram</li><hr />
+              <li>ORIC Office</li><hr />
+              <li>Journals</li><hr />
+              <li>Almuni</li><hr />
             </ul>
           </div>
         )}
 
         {/* Screen Blur */}
-        {menuOpen && <div className="screen-blur" onClick={handleMenuToggle}></div>}
+        {menuOpen && !closing && <div className="screen-blur" onClick={handleMenuToggle}></div>}
       </div>
     </div>
   );
